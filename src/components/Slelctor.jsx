823752127e5001e1ba7c251903806ccc7b140eb6
boon2187 <fumiya.mai.ito@gmail.com>
2022-07-23 08:32:23 +0900

    選ばれた国をstateにonChangeでいれる。で、それを渡す関数のひな形を作成

--- src/components/Slelctor.jsx
+++ src/components/Slelctor.jsx
@@ -5,13 +5,23 @@ import countriesJson from '../countries.json';
 
 const Selector = () => {
     const [country, setCountry] = useState("");
+
+    // selctorで選ばれた国のデータを受け取る
+    const getCountryData = () => {
+
+    }
+
+    // 実際のSelectorの部分
     return (
         <div>
-            <select>
+            <select onChange={(e) => setCountry(e.target.value)}>
                 {countriesJson.map((country, index) => 
-                    <option key={index} value={country.Slug}>{country.Country}</option>
+                    <option key={index} value={country.Slug} >
+                        {country.Country}
+                    </option>
                 )}
             </select>
+            {country}
         </div>
     );
 };
