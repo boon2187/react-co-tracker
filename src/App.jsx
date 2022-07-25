5a04d2080fcb6b16f131d7970670d81ccf671d2c
boon2187 <fumiya.mai.ito@gmail.com>
2022-07-26 08:07:27 +0900

    まず、TopPageコンポーネントにstateや関数を渡す

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div>
-      <TopPage />    
+      <TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData} />    
     </div>
   )
 }
