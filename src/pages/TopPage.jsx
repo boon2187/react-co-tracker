814302666100fca637d35d3a8dc8762327d26c3a
boon2187 <fumiya.mai.ito@gmail.com>
2022-07-26 08:10:58 +0900

    Appから受け取ったstateや関数をSelectorコンポーネントに渡す

--- src/pages/TopPage.jsx
+++ src/pages/TopPage.jsx
@@ -4,11 +4,11 @@ import Results from "../components/Results";
 import Selector from "../components/Selctor";
 import Title from "../components/Title";
 
-const TopPage = () => {
+const TopPage = (props) => {
     return (
         <div>
             <Title />
-            <Selector />
+            <Selector countriesJson={props.countriesJson} setCountry={props.setCountry} getCountryData={props.getCountryData} />
             <Results />
         </div>
       );
