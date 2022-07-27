405f118ccce51246a4bbdb3871005d5262b2fd7a
boon2187 <fumiya.mai.ito@gmail.com>
2022-07-27 13:57:50 +0900

    confirmの綴ミスを修正

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
   const getCountryData = () => {
       fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`).then(res => res.json()).then(data => { setCountryData({
         date: data[data.length -1].Date,
-        newConfirmed: data[data.length -1].Confrimed - data[data.length -2].Confrimed,
-        totalConfirmed: data[data.length -1].Confrimed,
+        newConfirmed: data[data.length -1].Confirmed - data[data.length -2].Confirmed,
+        totalConfirmed: data[data.length -1].Confirmed,
         newRecovered: data[data.length -1].Recovered - data[data.length -2].Recovered,
         totalRecovered: data[data.length -1].Recovered,
       }); });
@@ -28,6 +28,7 @@ function App() {
 
   return (
     <div>
+      {console.log(countryData)}
       <TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData} />    
     </div>
   )
