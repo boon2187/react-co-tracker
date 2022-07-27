381ca22a7b0cdcaf01be8ca1e420ab23087a66a6
boon2187 <fumiya.mai.ito@gmail.com>
2022-07-27 14:35:47 +0900

    countryDataの内容が表示されるように修正

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,11 +4,11 @@ const Results = (props) => {
     console.log("ResultsのProps", props)
     return (
         <div>
-            <p>日付：</p>
-            <p>新規感染者：</p>
-            <p>感染者総数：</p>
-            <p>新規回復者：</p>
-            <p>回復者総数：</p>
+            <p>日付：{props.countryData.date}</p>
+            <p>新規感染者：{props.countryData.newConfirmed}</p>
+            <p>感染者総数：{props.countryData.totalConfirmed}</p>
+            <p>新規回復者：{props.countryData.newRecovered}</p>
+            <p>回復者総数：{props.countryData.totalRecovered}</p>
         </div>
     );
 };
