bd46838a0e0d7dd0bc10bac8d1c008a7f0489af0
boon2187 <fumiya.mai.ito@gmail.com>
2022-07-28 11:09:21 +0900

    react-router-domを使って、ルートにTopPageを設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import TopPage from './pages/TopPage'
 import {useState} from 'react';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
 import countriesJson from './countries.json';
 
 function App() {
@@ -27,9 +28,12 @@ function App() {
   }
 
   return (
-    <div>
-      <TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData} countryData={countryData} />    
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TopPage countriesJson={countriesJson} setCountry={setCountry}  getCountryData={getCountryData} countryData={countryData} />} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
