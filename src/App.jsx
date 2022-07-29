a8d5ce9cce9deabf08f536e1088c2ec4ea00183c
boon2187 <fumiya.mai.ito@gmail.com>
2022-07-29 16:50:33 +0900

    WorldPageをApp.jsxに追加して/worldでページが遷移するように設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import TopPage from './pages/TopPage'
+import WorldPage from './pages/WorldPage';
 import {useState} from 'react';
 import {Route, Routes, BrowserRouter} from 'react-router-dom';
 import countriesJson from './countries.json';
@@ -33,7 +34,7 @@ function App() {
       <Routes>
         <Route path="/" element={<TopPage countriesJson={countriesJson} setCountry={setCountry}  getCountryData={getCountryData} countryData={countryData} />} />
         <Route path="/world" element={
-          <p>ワールド</p>
+          <WorldPage />
         } />
       </Routes>
     </BrowserRouter>
