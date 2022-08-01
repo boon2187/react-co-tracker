bff2c91110e402634ffb0939bd9ce57a9e9bf11f
boon2187 <fumiya.mai.ito@gmail.com>
2022-08-01 14:47:45 +0900

    全世界のデータを取得する関数getAllCountriesDataを作成

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,17 @@
 // Card.jsx
 
 const Card = () => {
+    // 全世界のデータを取得する
+    const getAllcountriesData = () => {
+        fetch("https://monotein-books.vercel.app/api/corona-tracker/summary")
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
+
     return(
         <div>
             <h2>Card</h2>
+            <button onClick={getAllcountriesData}>Get All Data</button>
         </div>
     );
 };
