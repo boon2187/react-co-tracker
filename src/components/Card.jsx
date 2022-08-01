8bc01840e9cb2d78c21a8e84d1dbb03f45d6be26
boon2187 <fumiya.mai.ito@gmail.com>
2022-08-01 14:48:52 +0900

    関数名getAllCountriesDataのミスを修正

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@
 
 const Card = () => {
     // 全世界のデータを取得する
-    const getAllcountriesData = () => {
+    const getAllCountriesData = () => {
         fetch("https://monotein-books.vercel.app/api/corona-tracker/summary")
             .then(res => res.json())
             .then(data => console.log(data))
@@ -11,7 +11,7 @@ const Card = () => {
     return(
         <div>
             <h2>Card</h2>
-            <button onClick={getAllcountriesData}>Get All Data</button>
+            <button onClick={getAllCountriesData}>Get All Data</button>
         </div>
     );
 };
