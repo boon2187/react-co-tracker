d082a4f2d4296e4381b9c11a9f644dab4d99beb4
boon2187 <fumiya.mai.ito@gmail.com>
2022-08-03 08:36:30 +0900

    stateに入れた値からデータの表示まで完了。次は、最上位コンポーネントにstateを移すか…

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,9 +14,16 @@ const Card = () => {
 
     return(
         <div>
-            {console.log(allCountriesData)}
             <h2>Card</h2>
             <button onClick={getAllCountriesData}>Get All Data</button>
+            {allCountriesData.map((singleData, index) => 
+                <div key={index}>
+                    {console.log(singleData)}
+                    <h2>{singleData.Country}</h2>
+                    <p>新規感染者：{singleData.NewConfirmed}</p>
+                    <p>感染者総数：{singleData.TotalConfirmed}</p>
+                </div>
+            )}
         </div>
     );
 };
