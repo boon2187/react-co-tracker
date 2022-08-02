51ab130c15af8a00cfb747a9a25882bcef051b15
boon2187 <fumiya.mai.ito@gmail.com>
2022-08-03 08:12:53 +0900

    state

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,15 @@
 // Card.jsx
+import { useState } from 'react'
 
 const Card = () => {
+    // とりあえず、Cardコンポーネント内に全世界のデータを保存するstateを作っておく
+    const [allCountriesData, setAllCountriesData] = useState([]);
+
     // 全世界のデータを取得する
     const getAllCountriesData = () => {
         fetch("https://monotein-books.vercel.app/api/corona-tracker/summary")
             .then(res => res.json())
-            .then(data => console.log(data.Countries))
+            .then(data => setAllCountriesData(data.Countries))
     }
 
     return(
