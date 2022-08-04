509d40b23d537e2be08d168163e9f4ebdd160277
boon2187 <fumiya.mai.ito@gmail.com>
2022-08-04 14:28:54 +0900

    各ページにHeaderコンポーネントを追加。classNameを追加して見た目を整理して完成

--- src/pages/TopPage.jsx
+++ src/pages/TopPage.jsx
@@ -1,5 +1,6 @@
 // TopPage.jsx
 
+import Header from "../components/Header";
 import Results from "../components/Results";
 import Selector from "../components/Selctor";
 import Title from "../components/Title";
@@ -10,6 +11,7 @@ const TopPage = (props) => {
     return (
         <div className="top-page-container">
             <div>
+                <Header />
                 <Title />
                 <Selector countriesJson={props.countriesJson} setCountry={props.setCountry} getCountryData={props.getCountryData} />
                 <Results countryData={props.countryData} />
