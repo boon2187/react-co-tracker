2f763bbd99243d5f6f6cdbc18c65a7e50e8889fe
boon2187 <fumiya.mai.ito@gmail.com>
2022-08-05 10:36:03 +0900

    getCountryDataをuseEffect何に移動

--- src/App.jsx
+++ src/App.jsx
@@ -23,31 +23,30 @@ function App() {
   // 通信中かどうかの状態を保存しておくstate
   const [loading, setLoading] = useState(false);
 
-  // その国データを受け取る  
-  const getCountryData = () => {
-    // 通信中に設定
-    setLoading(true);
-
-    // 指定された国のデータを取得
-    fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
-    .then(res => res.json())
-    .then(data => { 
-      setCountryData({
-        date: data[data.length -1].Date,
-        newConfirmed: data[data.length -1].Confirmed - data[data.length -2].Confirmed,
-        totalConfirmed: data[data.length -1].Confirmed,
-        newRecovered: data[data.length -1].Recovered - data[data.length -2].Recovered,
-        totalRecovered: data[data.length -1].Recovered,
-      }); 
-
-      // 通信中であることを解除
-      setLoading(false);
-    }).catch(err => alert("エラーが発生しました。ページをリロードして、もう一度トライしてください。"))
-  }
-
+  
   // useEffectを使って、国名を選んだだけで、データを取得できるようにする
   useEffect(() => {
-    
+    // その国データを受け取る  
+    const getCountryData = () => {
+      // 通信中に設定
+      setLoading(true);
+  
+      // 指定された国のデータを取得
+      fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
+      .then(res => res.json())
+      .then(data => { 
+        setCountryData({
+          date: data[data.length -1].Date,
+          newConfirmed: data[data.length -1].Confirmed - data[data.length -2].Confirmed,
+          totalConfirmed: data[data.length -1].Confirmed,
+          newRecovered: data[data.length -1].Recovered - data[data.length -2].Recovered,
+          totalRecovered: data[data.length -1].Recovered,
+        }); 
+  
+        // 通信中であることを解除
+        setLoading(false);
+      }).catch(err => alert("エラーが発生しました。ページをリロードして、もう一度トライしてください。"))
+    }
   })
 
   
